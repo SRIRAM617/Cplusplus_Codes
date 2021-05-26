bed97e64b22eee327256e5c60afdfd0a1d92939c
SRIRAM617 <65893800+SRIRAM617@users.noreply.github.com>
2021-05-26 22:17:50 +0530

    Create ReverseANumber.cpp

---
+++ ReverseANumber.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n,r;
+  cout<<"Enter a number"<<endl;
+  cin>>n;
+  
+  while(n>0)
+  {
+      r=n%10;
+      n=n/10;
+      
+      cout<<r;
+     
+      
+  }
+}
+  
